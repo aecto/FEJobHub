c0c5e6b6b9745771a308a4cc108303f94753ca4e
aecto <aecto@163.com>
2025-09-05 20:51:47 +0800

    modify job detail layout

--- frontend/src/components/job/JobDetail.jsx
+++ frontend/src/components/job/JobDetail.jsx
@@ -75,12 +75,17 @@ const JobDetail = ({ job }) => {
           <span className="job-location">{job.location}</span>
           <span className="job-salary">{job.salary_range || '面议'}</span>
           <span className="job-posted">发布于 {formatRelativeTime(job.created_at)}</span>
-          <button className="share-button" onClick={handleShare} title="分享职位">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M11 6.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2v7h9v-7h-2a.5.5 0 0 1-.5-.5z"/>
-              <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
-            </svg>
-          </button>
+          <div className="button-group">
+            <button className="apply-button-small" onClick={handleApply}>
+              官网链接
+            </button>
+            <button className="share-button" onClick={handleShare} title="分享职位">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M11 6.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2v7h9v-7h-2a.5.5 0 0 1-.5-.5z"/>
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
       
@@ -98,11 +103,7 @@ const JobDetail = ({ job }) => {
         )}
       </div>
       
-      <div className="job-actions">
-        <button className="apply-button" onClick={handleApply}>
-          官方申请
-        </button>
-      </div>
+      {/* 底部的官方申请按钮已移至顶部，此处删除 */}
     </div>
   )
 }
