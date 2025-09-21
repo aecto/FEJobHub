d4d1e0e59c6ce366f6d1abad3d7e3fe8290af44c
aecto <aecto@163.com>
2025-09-21 22:23:15 +0800

    Prepre upload to git hub remove some evn configuration

--- mysql/init/01-init.sql
+++
@@ -1,5 +0,0 @@
--- 创建数据库和用户
-CREATE DATABASE IF NOT EXISTS fejobhub;
-CREATE USER IF NOT EXISTS 'fejobhub_user'@'%' IDENTIFIED BY 'fejobhub_password';
-GRANT ALL PRIVILEGES ON fejobhub.* TO 'fejobhub_user'@'%';
-FLUSH PRIVILEGES;
